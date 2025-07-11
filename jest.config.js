8867862d473709e830adb65515a685471247aa8e
Davide Casale <davi0kprogramsthings@gmail.com>
2025-07-12 00:07:08 +0200

    Remove broken unit tests.

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  globalSetup: process.env.SKIP_SETUP ? undefined : './tests/jest.setup.js',
-  globalTeardown: process.env.SKIP_TEARDOWN ? undefined : './tests/jest.teardown.js',
-  testEnvironment: 'node',
-  testTimeout: 30_000
-}
